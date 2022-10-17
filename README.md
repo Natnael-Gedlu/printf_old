f5dddd7c15041b777cd34e89d1779ba781ad7cee
Natnael-Gedlu <nategd800@gmail.com>
2022-10-17 08:11:49 -0700

    README.md added

---
+++ README.md
@@ -0,0 +1,4 @@
+alx project 0x11. C-printf group project
+Team: Natnael Gedlu, Hlamulo Chauke
+
+0. A function that produces output according to a format.
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - header file containing prototypes for functions used 
+ * in Project 0x11. C-printf
+ * Team - Natnael Gedlu & Hlamulo Chauke.
+ */
+
+int _printf(const char *format, ...);
+
+#endif
