969fce79518fcd3d706cf2dcff438bb5a09f9019
Natnael-Gedlu <nategd800@gmail.com>
2022-10-19 02:58:09 -0700

    _printf.c update

--- _putchar.c
+++ _putchar.c
@@ -4,6 +4,8 @@
  * _putchar - putchar used for functions used 
  * in Project 0x11. C-printf
  * Team - Natnael Gedlu & Hlamulo Chauke.
+ * @c: the character to print
+ * Return: 0 on success and -1 error and errno is set appropiately
  */
 int _putchar(char c)
 {
--- main.h
+++ main.h
@@ -11,7 +11,6 @@
  * Team - Natnael Gedlu & Hlamulo Chauke.
  */
 
-int _putchar(char c);
 int print_int(va_list arg);
 int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
@@ -22,20 +21,4 @@ void print_binary(unsigned int n, unsigned int *printed);
 int print_unsignedToBinary(va_list arg);
 int print_oct(va_list arg);
 int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR(va_list arg);
 
-/**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- */
-typedef struct identifierStruct
-{
-char *indentifier;
-int (*printer)(va_list);
-} identifierStruct;
-
-#endif
